ca713cd99bd00e3154a928016e812c511097884f
Autoolize <sadig@ymail.com>
2024-09-18 20:25:39 +0000

    Update from https://github.com/Autoolize/django-static/commit/9e9957fe34f0edbb99a1455e4cd41b5dd95ef0e1

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,23 @@
     $('.navigation-btn-toggle').on('click', function () {
         $('#mobile-menu').toggleClass('hidden')
     });
+
+    // Tab click handler
+    $(".flex button").on("click", function () {
+        // Remove active class and border from all tabs
+        $(".flex button").removeClass("border-blue-600 text-blue-600");
+        $(".flex button").addClass("border-transparent");
+
+        // Add active class and border to the clicked tab
+        $(this).removeClass("border-transparent").addClass("border-blue-600 text-blue-600");
+
+        // Get the target content ID from the clicked tab's data-target attribute
+        let target = $(this).data("target");
+
+        // Hide all content sections
+        $(".tab-content").addClass("hidden");
+
+        // Show the corresponding content section
+        $(target).removeClass("hidden");
+    });
 })(jQuery)
